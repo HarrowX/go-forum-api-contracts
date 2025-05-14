840833f9b934af30d7b4485d51e52c1b35341a8c
HarrowX05 <sashok_play@mail.ru>
2025-05-14 23:38:41 +0400

    change generate.sh and add generated files

--- generate.sh
+++ generate.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Создаем целевую директорию
 mkdir -p pkg/proto
 
-# Генерируем в pkg/proto
 protoc --go_out=pkg/proto --go_opt=paths=source_relative \
        --go-grpc_out=pkg/proto --go-grpc_opt=paths=source_relative \
        proto/auth.proto
-
-echo "Файлы сгенерированы в pkg/proto"
